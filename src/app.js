ec3294d0408b29afd98fde4ed33cb8c270585a0f
Cryptk1ddo <91121293+Cryptk1ddo@users.noreply.github.com>
2025-02-06 13:35:18 +0100

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScreenRecorderApp from './components/ScreenRecorderApp';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <ScreenRecorderApp />
+    </div>
+  );
+}
+
+export default App;
